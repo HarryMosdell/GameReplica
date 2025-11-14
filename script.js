4dfb2e97215ac87a29965114fe9ebff2cfbe7de1
HarryMosdell <up712364@myport.ac.uk>
2025-11-14 04:53:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -48,22 +48,28 @@ let slides =document.querySelectorAll(".slide");
  let counter=0;
  let maxSteps=1;
  let movement=-25;
- if (window.innerWidth <= 800) {
-     maxSteps=2;
-     movement=-55;
-} else {
-     maxSteps=1;
-     movement=-30;
+
+
+function sliderConfig() {
+    if (window.innerWidth <= 800) {
+        maxSteps=2;
+        movement=-55;
+       
+   } else {
+        maxSteps=1;
+        movement=-30;
+   }
+
 }
+document.addEventListener('DOMContentLoaded',sliderConfig);
+window.addEventListener('resize',sliderConfig);
 
 left_arrow.addEventListener('click', ()=> {
      counter--;  
   
      if(counter<0) {
-        counter=1;
-        if (window.innerWidth <= 800) {
-           counter=2;
-       }
+        counter=maxSteps;
+        
      }
      updateSliderPosition();
 })
@@ -83,3 +89,98 @@ function updateSliderPosition(){
      
     
 }
+
+const header = document.querySelector('header');
+
+function load() {
+    items = JSON.parse(localStorage.getItem('items'));
+    if(!items) {
+    items= [];
+    }
+}
+
+function render_basket () {
+ 
+    let container= document.querySelector('.game-i');
+    container.innerHTML = '';
+    if(items.length>0){
+    items.forEach( (item)=> {
+
+
+let game_info= document.createElement('div');
+ 
+
+container.appendChild(game_info);
+game_info.classList.add('d-flex')
+let game_price=document.createElement('p');
+
+let game_text=document.createElement('p');
+game_info.style.gap= 5 +'px';
+game_text.textContent=item.name;
+game_price.textContent=  ` £${item.price * item.qty} ` ;
+game_info.appendChild(game_text);
+game_info.appendChild(game_price);
+});
+}
+
+else {
+ let con= document.querySelector('.game-i');
+ let emptyMessage= document.createElement('p');
+ emptyMessage.textContent="no items in the basket!"
+ con.appendChild(emptyMessage);
+}
+
+  }
+
+window.addEventListener('pageshow', load);
+window.addEventListener('pageshow', render_basket);
+
+document.addEventListener('DOMContentLoaded', load);
+document.addEventListener('DOMContentLoaded', render_basket);
+
+gamesList=[{name:'PS5 Limited Controller', img:'https://cdn.media.amplience.net/i/frasersdev/DUALSENSE_LE_ASTROBOT_2.0_BEAUTY_01_CROP_RGB?fmt=auto&upscale=false&w=451&h=451&sm=c&$h-ttl$'
+, id:5, price:'45', qty:1},{name:'007 First Light', img:'https://cdn.media.amplience.net/i/frasersdev/007_first_light_1x1?fmt=auto&upscale=false&w=451&h=451&sm=c&$h-ttl$', id:1, price:'65', qty:1} 
+,{name:'Super Mario', img:'https://cdn.media.amplience.net/i/frasersdev/super_mario_galaxy_1and2_1x1?fmt=auto&upscale=false&w=451&h=451&sm=c&$h-ttl$', id:6,  price:'70', qty:1}, ];
+
+let gameText= document.querySelectorAll('.gameText');
+let addButtons= document.querySelectorAll('.addButton');
+
+gamesList.forEach((game,index)=> {
+    if (gameText[index]) {
+    gameText[index].innerText=game.name; 
+    }
+
+    if(addButtons[index]) {
+        addButtons[index].setAttribute('id', game.id) 
+    }
+} );
+
+addButtons.forEach((addButton)=> {
+
+    addButton.addEventListener('click', ()=> {
+
+        addToBasket(Number(addButton.getAttribute('id')));
+    } )
+
+} )
+
+
+function addToBasket(id) {
+  
+    let gameToAdd= gamesList.find(game => game.id === id);
+  
+  let found= items.find(i => i.id === id);
+  
+  if(found) {
+      found.qty++;
+      render_basket();
+  }
+  else {
+      items.push(gameToAdd);
+      render_basket();
+  }
+
+  localStorage.setItem('items', JSON.stringify(items));
+  alert(localStorage.getItem('items'));
+  
+  }
